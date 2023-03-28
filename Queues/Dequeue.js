21e45a913c5af01fff4754523ffef8bdf3ce98dc
Harsh <hparashar617@gmail.com>
2023-03-28 23:34:10 +0530

    dequeue enqueue setup

--- Queues/Dequeue.js
+++ Queues/Dequeue.js
@@ -12,7 +12,20 @@ this.val = val;
 this.next = next;
     }
 
- 
+  dequeue(){
+    if(!this.first){
+        return undefined
+    }
+    let temp = this.first;
+    if(this.first === this.last){
+        this.last = null
+    }
+    else{
+        this.first = this.first.next;
+        this.size--;
+    }
+    return temp.val;
+  }  
 }
 
 // the Queue is abstract data structure based on the principle first in first out ->
