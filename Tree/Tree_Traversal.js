2ad8a3a01c42c72c842456620c33cd305899b7ef
Harsh <hparashar617@gmail.com>
2023-04-05 22:35:46 +0530

    Trees Traversal

---
+++ Tree/Tree_Traversal.js
@@ -0,0 +1,21 @@
+// Acording to this javascript course there are two ways of traversing the tree ->
+
+// Breadth first search ->
+// In this type of tree traversal, traverse the tree by level by level
+/*
+the algo for the BST iteratively ->
+
+1 - Create a Queue (using an array as well) and variable to store the value of the visited node
+2 - Place the root node in the queue, Loop untill the queue get ended 
+3 - Dequeue a node from the queue and push the value of node into the variable that store the node
+4- if there is a left property on the node dequeued add it to queue
+5 - if there is a right property on the node dequeued add it to the queue
+6- return the variable that stores the value  
+
+*/
+
+// Depth First search -> In this type of traversal three types of the traversal comes 
+// DEPTH PREORDER TRAVERSAL 
+// DEPTH INORDER TRAVERSAL 
+// DEPTH POSTORDER TRAVERSAL 
+
