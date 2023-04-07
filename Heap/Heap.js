32ee81752b132adc63e457b0aaf50266472840da
Harsh <hparashar617@gmail.com>
2023-04-07 15:48:19 +0530

    Binary heap

--- Heap/Heap.js
+++ Heap/Heap.js
@@ -1,4 +1,12 @@
-// This is all about the Binary heap , 
+// This is all about the Binary heap 
+/*
+
+in the case of the creation of the heap for the n th position index the 
+right node -> 2n+2
+left node -> 2n+1
+
+ */
+
 // max heap 
 // min heap 
 // heapify
