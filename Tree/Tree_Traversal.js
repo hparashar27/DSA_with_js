91a229ce5ab6cb3945c57129cc71c4d5d0b438f4
Harsh <hparashar617@gmail.com>
2023-04-07 09:46:10 +0530

    DFS Traversal algo

--- Tree/Tree_Traversal.js
+++ Tree/Tree_Traversal.js
@@ -15,8 +15,56 @@ the algo for the BST iteratively ->
 
 // the code for this traversal is in the BST.js file ->
 
-// Depth First search -> In this type of traversal three types of the traversal comes 
-// DEPTH PREORDER TRAVERSAL 
+// Depth First search -> 
+// In this type of traversal the tree is traverse in the vertical direction from top to bottom or bottom to top ->
+
+
+// In this type of traversal three types of the traversal comes in 
+
+// DEPTH PREORDER TRAVERSAL -
+
+/* 
+1- Create a variable  to store value of the nodes visited 
+2- Store the root in the variable called current 
+3- Write a helper function that accepts a node
+   *Push the value of node in the variable that store the values
+   *if the node has left property ,call the helper function with the left property on the node
+   *if the node has right property ,call the helper function with the right property on the node
+4- Invoke the helper function with the current variable
+5- Return the array of values
+   /
+
 // DEPTH INORDER TRAVERSAL 
+
+/*
+1- Create a variable  to store value of the nodes visited 
+2- Store the root in the variable called current 
+3- Write a helper function that accepts a node
+   *if the node has left property ,call the helper function with the left property on the node
+   *Push the value of node in the variable that store the values
+   *if the node has right property ,call the helper function with the right property on the node
+4- Invoke the helper function with the current variable
+5- Return the array of values
+*/
+
 // DEPTH POSTORDER TRAVERSAL 
+
+/*
+1- Create a variable  to store value of the nodes visited 
+2- Store the root in the variable called current 
+3- Write a helper function that accepts a node
+   *if the node has left property ,call the helper function with the left property on the node
+   *if the node has right property ,call the helper function with the right property on the node
+   *Push the value of node in the variable that store the values
+4- Invoke the helper function with the current variable
+5- Return the array of values
+*/
+
+
+// BFS VS DFS : WHICH ONE IS BEST ->
+/*
+So in terms of the time complexity both has equal time complexity ,and in terms of space complexity depends on the tree structure ->
+so it depends on the type of structure if the depth of the tree is very deep then the DFS is not a good approach 
+
+*/
 
