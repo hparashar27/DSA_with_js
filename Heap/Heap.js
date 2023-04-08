8b7e56f1e22afb7cded4fe62efd00b719d295f64
Harsh <hparashar617@gmail.com>
2023-04-08 21:23:19 +0530

    Hashing hash table

--- Heap/Heap.js
+++ Heap/Heap.js
@@ -5,6 +5,9 @@ in the case of the creation of the heap for the n th position index the
 right node -> 2n+2
 left node -> 2n+1
 
+for any child at index n , then its parent is at index 
+=> (n-1)/2
+
  */
 
 // max heap 
