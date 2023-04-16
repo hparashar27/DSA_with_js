98d52ab0a42b0aa17230b78e874d067799c87a7e
Harsh <hparashar617@gmail.com>
2023-04-16 17:57:41 +0530

    dp theory

---
+++ Dynamic Programming/Dp.js
@@ -0,0 +1,16 @@
+// WTF IS DYNAMIC PROGRAMMING
+
+/*
+"A method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions."
+"A method for solving a complex problem by breaking it down into a collection of simpler subproblems, solving each of those subproblems just once, and storing their solutions."
+
+DP - Optimal solution + Overlapping subproblems
+
+OPTIMAL SUBSTRUCTURE -> A problem is said to have optimal substructure if an optimal solution can be constructed from optimal solutions of its subproblems
+
+OVERLAPPING SUBPROBLEMS -> A problem is said to have overlapping subproblems if it can be broken down into subproblems which are reused several times
+
+for example if we talk about the fibbncaci series then when we solve it with recurssion then its time complexity is of (2^n)
+
+
+ */
