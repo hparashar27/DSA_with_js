6d9d4a56ded227650aa2e6e911abbc3022f90f09
Harsh <hparashar617@gmail.com>
2023-04-18 08:45:15 +0530

    remveEdge

--- Graphs/Graph.js
+++ Graphs/Graph.js
@@ -4,16 +4,27 @@ class Graph {
     constructor(){
         this.adjacencyList = {}; // make a object named for the adjacencyList  
     }
+
+
     addVertex(vertex){
         if(!this.adjacencyList[vertex]){
             this.adjacencyList[vertex] = []
         }
     }
+
     addEdge(vertex1,vertex2){
         // this is for the undirected graph ->
 this.adjacencyList[vertex1].push(vertex2);
 this.adjacencyList[vertex2].push(vertex1);
+    }
 
+    removeEdge(v1,v2){
+ this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+            v  => v !== v2
+        )
+ this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+            v  => v !== v1
+        )
     }
 }
 
