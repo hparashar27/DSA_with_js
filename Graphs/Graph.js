5699e77a83f5e6ff6549cd050b48507da883ceff
Harsh <hparashar617@gmail.com>
2023-04-18 08:21:00 +0530

    addedge

--- Graphs/Graph.js
+++ Graphs/Graph.js
@@ -1,7 +1,5 @@
 // Adjacency list in the graph 
 
-const { version } = require("react");
-
 class Graph {
     constructor(){
         this.adjacencyList = {}; // make a object named for the adjacencyList  
@@ -11,4 +9,21 @@ class Graph {
             this.adjacencyList[vertex] = []
         }
     }
+    addEdge(vertex1,vertex2){
+        // this is for the undirected graph ->
+this.adjacencyList[vertex1].push(vertex2);
+this.adjacencyList[vertex2].push(vertex1);
+
+    }
 }
+
+let g = new Graph()
+g.addVertex("delhi");
+g.addVertex("banglore");
+g.addVertex("mumbai");
+g.addVertex("guwahti");
+g.addEdge("delhi","mumbai");
+g.addEdge("banglore","mumbai");
+g.addEdge("delhi","guwahti");
+g.addEdge("mumbai","banglore");
+
