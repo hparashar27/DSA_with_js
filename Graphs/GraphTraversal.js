75ba0bb92c628febb662959881f68142d23c95f5
Harsh <hparashar617@gmail.com>
2023-04-20 22:38:32 +0530

    GraphTraversal.js BFS

--- Graphs/GraphTraversal.js
+++ Graphs/GraphTraversal.js
@@ -58,5 +58,17 @@ Loop over all of the values in the adjacencyList for that vertex
 If any of those values have not been visited, invoke the helper function with that vertex
 return the array of results
 
+BREADTH FIRST 
+
+This function should accept a starting vertex
+Create a queue (you can use an array) and place the starting vertex in it
+Create an array to store the nodes visited
+Create an object to store nodes visited
+Mark the starting vertex as visited
+Loop as long as there is anything in the queue
+Remove the first vertex from the queue and push it into the array that stores nodes visited
+Loop over each vertex in the adjacency list for the vertex you are visiting.
+If it is not inside the object that stores nodes visited, mark it as visited and enqueue that vertex
+Once you have finished looping, return the array of visited nodes
 */
 
