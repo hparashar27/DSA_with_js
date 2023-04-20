46d845c7c6642bbef13bf9f5912f0876e0f7c054
Harsh <hparashar617@gmail.com>
2023-04-20 22:37:15 +0530

    GraphTraversal DFS

--- Graphs/GraphTraversal.js
+++ Graphs/GraphTraversal.js
@@ -15,7 +15,11 @@ DFS Traversal psudocode ->
           recursively call DFS on neighbor
 
  Recursive way of DFS
- Algo - 
+
+ **************************************** Algo **********************************************
+
+( By recursice -> )
+
  * The function should accept a starting node 
  * Create a list to store the end result, to be returned at the very end
  * Create an object to store visited vertices
@@ -27,5 +31,32 @@ If any of those values have not been visited, recursively
 * invoke the helper function with that vertex
 * Return the result array
 
-        
+***************************************** Algo **********************************************
+
+( By iterative -> )
+
+The function should accept a starting node
+Create a stack to help use keep track of vertices (use a list/array)
+Create a list to store the end result, to be returned at the very end
+Create an object to store visited vertices
+Add the starting vertex to the stack, and mark it visited
+While the stack has something in it:
+Pop the next vertex from the stack
+If that vertex hasn't been visited yet:
+​Mark it as visited
+Add it to the result list
+Push all of its neighbors into the stack
+Return the result array
+    
+DEPTH FIRST TRAVERSAL
+
+The function should accept a starting node
+Create an object to store visited nodes and an array to store the result
+Create a helper function which accepts a vertex
+The helper function should place the vertex it accepts into the visited object and push that vertex into the results
+Loop over all of the values in the adjacencyList for that vertex
+If any of those values have not been visited, invoke the helper function with that vertex
+return the array of results
+
 */
+
