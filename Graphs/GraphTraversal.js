2d70002ec41909436656b89c128f7cc34d5be64d
Harsh <hparashar617@gmail.com>
2023-04-21 09:07:18 +0530

    GraphTraversal DFS code

--- Graphs/GraphTraversal.js
+++ Graphs/GraphTraversal.js
@@ -72,3 +72,71 @@ If it is not inside the object that stores nodes visited, mark it as visited and
 Once you have finished looping, return the array of visited nodes
 */
 
+
+class Graph {
+   constructor(){
+       this.adjacencyList = {}; // make a object named for the adjacencyList  
+   }
+
+
+   addVertex(vertex){
+       if(!this.adjacencyList[vertex]){
+           this.adjacencyList[vertex] = []
+       }
+   }
+
+   addEdge(vertex1,vertex2){
+       // this is for the undirected graph ->
+this.adjacencyList[vertex1].push(vertex2);
+this.adjacencyList[vertex2].push(vertex1);
+   }
+
+   removeEdge(v1,v2){
+this.adjacencyList[v1] = this.adjacencyList[v1].filter(
+           v  => v !== v2
+       )
+this.adjacencyList[v2] = this.adjacencyList[v2].filter(
+           v  => v !== v1
+       )
+   }
+
+   removeVertex(ver){
+       while(this.adjacencyList[ver].length){
+           this.adajacentVertex = this.adjacencyList[ver].pop();
+           this.removeEdge(ver,adajacentVertex)
+       }
+       delete this.adjacencyList[ver]
+   }
+
+   dfsTraversal(start){
+let result = [];
+let visited = {};
+const adjacencyList = this.adjacencyList;
+
+function dfs(vertex){
+if(!vertex){
+   return null;
+}
+visited[vertex] = true;
+result.push(vertex);
+adjacencyList[vertex].forEach(neighbor => {
+   if(!visited[neighbor]){
+      return dfs(neighbor)
+   }
+});
+}
+dfs(start);
+return result
+   }
+}
+
+let g = new Graph()
+g.addVertex("delhi");
+g.addVertex("banglore");
+g.addVertex("mumbai");
+g.addVertex("guwahti");
+g.addEdge("delhi","mumbai");
+g.addEdge("banglore","mumbai");
+g.addEdge("delhi","guwahti");
+g.addEdge("mumbai","banglore");
+
