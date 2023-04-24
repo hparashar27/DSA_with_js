0f27fcdc9e74ed8bdadff484b39fbe4cf0b9e31f
Harsh <hparashar617@gmail.com>
2023-04-24 21:01:00 +0530

    GraphTraversal BFS Traversal

--- Graphs/GraphTraversal.js
+++ Graphs/GraphTraversal.js
@@ -177,6 +177,41 @@ dfsTraversalIterative(start){
         })    }
     return result ;
 }
+
+// BFS traversal of the graph -> level order traversal , visit neighbor of neighbor 
+
+/*
+- This function should accept a starting vertex
+- Create a queue (you can use an array) and place the starting vertex in it
+- Create an array to store the nodes visited
+- Create an object to store nodes visited
+- Mark the starting vertex as visited
+- Loop as long as there is anything in the queue
+- Remove the first vertex from the queue and push it into the array that stores nodes visited
+- Loop over each vertex in the adjacency list for the vertex you are visiting.
+- If it is not inside the object that stores nodes visited, mark it as visited and enqueue that vertex
+- Once you have finished looping, return the array of visited nodes
+*/
+
+bfsTraversal(start){
+    const queue = [start];
+    const result = [];
+    const visited = {};
+    let CurrentVertex ;
+    visited[start] = true;
+    while(queue.length){
+    CurrentVertex = queue.shift();
+    result.push(CurrentVertex);
+    this.adjacencyList[vertex].forEach(neighbor =>{
+        if(!visited[neighbor]){
+            queue.push(neighbor);
+        }
+    })
+    }
+    return result
+}
+ 
+
 }
 
 let g = new Graph()
