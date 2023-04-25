43c1453fdbbff6bf93ee66fa2b7ebe48e6ba8ae8
Harsh <hparashar617@gmail.com>
2023-04-25 19:14:18 +0530

    Dijkstra algo objective

---
+++ Dijkstra's Algorithim/DijkstraAlgo.js
@@ -0,0 +1,12 @@
+// Dijkstra's Algorithm ->
+
+/*
+
+OBJECTIVES
+Understand the importance of Dijkstra's
+Implement a Weighted Graph
+Walk through the steps of Dijkstra's
+Implement Dijkstra's using a naive priority queue
+Implement Dijkstra's using a binary heap priority queue
+
+*/
