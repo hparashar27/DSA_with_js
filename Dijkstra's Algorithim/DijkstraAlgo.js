0b4bae46b55e30578b25611ad5d37b9cb1920cc6
Harsh <hparashar617@gmail.com>
2023-04-25 19:16:41 +0530

    Dijkstra algo aim and uses

--- Dijkstra's Algorithim/DijkstraAlgo.js
+++ Dijkstra's Algorithim/DijkstraAlgo.js
@@ -9,4 +9,13 @@ Walk through the steps of Dijkstra's
 Implement Dijkstra's using a naive priority queue
 Implement Dijkstra's using a binary heap priority queue
 
+Aim - Finds the shortest path between two vertices on a graph
+
+WHY IS IT USEFUL?
+GPS - finding fastest route
+Network Routing - finds open shortest path for data
+Biology - used to model the spread of viruses among humans
+Airline tickets - finding cheapest route to your destination
+Many other uses!
+
 */
