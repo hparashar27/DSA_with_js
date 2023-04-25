6498ca100b837c680dcb96b48955baa5557ee6b9
Harsh <hparashar617@gmail.com>
2023-04-25 19:18:18 +0530

    Dijkstra algo approach

--- Dijkstra's Algorithim/DijkstraAlgo.js
+++ Dijkstra's Algorithim/DijkstraAlgo.js
@@ -18,4 +18,12 @@ Biology - used to model the spread of viruses among humans
 Airline tickets - finding cheapest route to your destination
 Many other uses!
 
+Approach ->
+
+Every time we look to visit a new node, we pick the node with the smallest known distance to visit first.
+Once we’ve moved to the node we’re going to visit, we look at each of its neighbors
+For each neighboring node, we calculate the distance by summing the total edges that lead to the node we’re checking from the starting node.
+If the new total distance to a node is less than the previous total, we store the new shorter distance for that node.
+
+
 */
